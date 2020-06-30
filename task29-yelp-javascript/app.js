75fcdf9aeb4e8b507a9c1254a137feb88217ba36
ritheesh4 <ritheesh.4@gmail.com>
2020-06-30 15:44:05 +0530

    code optimised task 29.1

--- task29-yelp-javascript/app.js
+++ task29-yelp-javascript/app.js
@@ -1,3 +1,7 @@
+const star = [0,1,1.5,2,2.5,3,3.5,4,4.5,5];
+// const position = ["425px","388px","350px","315px","278px","243px","206px","133px","170px","133px","97px"];
+// let a = star.indexOf(3.5);
+// console.log(position[a])
 
 function loadData(e) {
     xhr = new XMLHttpRequest();
@@ -9,56 +13,63 @@ function loadData(e) {
             data.forEach(element => {
                 document.getElementById(element.id).innerHTML = `<div>${element.review}</div>`;
                 document.getElementById(`w-${element.id}`).innerHTML = `<span>${element.weeks}</span>`;
-                
-                if (element.stars === 5) {
-                    const img = document.getElementById(`img-${element.id}`);
 
-                    img.style.height = "97px";
-                }
-                else if (element.stars === 4.5) {
-                    const img = document.getElementById(`img-${element.id}`);
+                let starCount = star.indexOf(element.stars);
+                let position = `${425-starCount*36.5}px`;
+                console.log(position)
+                const img = document.getElementById(`img-${element.id}`);
 
-                    img.style.height = "133px";
-                } else if (element.stars === 4) {
-                    const img = document.getElementById(`img-${element.id}`);
+                img.style.height = position;
 
-                    img.style.height = "170px";
-                }
-                else if (element.stars === 3.5) {
-                    const img = document.getElementById(`img-${element.id}`);
+                // if (element.stars === 5) {
+                //     const img = document.getElementById(`img-${element.id}`);
 
-                    img.style.height = "206px";
-                }
-                else if (element.stars === 3) {
-                    const img = document.getElementById(`img-${element.id}`);
+                //     img.style.height = "97px";
+                // }
+                // else if (element.stars === 4.5) {
+                //     const img = document.getElementById(`img-${element.id}`);
 
-                    img.style.height = "243px";
-                }
-                else if (element.stars === 2.5) {
-                    const img = document.getElementById(`img-${element.id}`);
+                //     img.style.height = "133px";
+                // } else if (element.stars === 4) {
+                //     const img = document.getElementById(`img-${element.id}`);
 
-                    img.style.height = "278px";
-                }
-                else if (element.stars === 2) {
-                    const img = document.getElementById(`img-${element.id}`);
+                //     img.style.height = "170px";
+                // }
+                // else if (element.stars === 3.5) {
+                //     const img = document.getElementById(`img-${element.id}`);
 
-                    img.style.height = "315px";
-                }
-                else if (element.stars === 1.5) {
-                    const img = document.getElementById(`img-${element.id}`);
+                //     img.style.height = "206px";
+                // }
+                // else if (element.stars === 3) {
+                //     const img = document.getElementById(`img-${element.id}`);
 
-                    img.style.height = "350px";
-                }
-                else if (element.stars === 1) {
-                    const img = document.getElementById(`img-${element.id}`);
+                //     img.style.height = "243px";
+                // }
+                // else if (element.stars === 2.5) {
+                //     const img = document.getElementById(`img-${element.id}`);
 
-                    img.style.height = "388px";
-                }
-                else if (element.stars < 1) {
-                    const img = document.getElementById(`img-${element.id}`);
+                //     img.style.height = "278px";
+                // }
+                // else if (element.stars === 2) {
+                //     const img = document.getElementById(`img-${element.id}`);
 
-                    img.style.height = "425px";
-                }
+                //     img.style.height = "315px";
+                // }
+                // else if (element.stars === 1.5) {
+                //     const img = document.getElementById(`img-${element.id}`);
+
+                //     img.style.height = "350px";
+                // }
+                // else if (element.stars === 1) {
+                //     const img = document.getElementById(`img-${element.id}`);
+
+                //     img.style.height = "388px";
+                // }
+                // else if (element.stars < 1) {
+                //     const img = document.getElementById(`img-${element.id}`);
+
+                //     img.style.height = "425px";
+                // }
 
             });
         }
